b34c07514907f27fe4e12fbc24b36713dc28ec8f
Hubertus van Dam <hvandam@bnl.gov>
2019-06-06 23:40:07 -0400

    HvD: jobscripts to test all variants of LDA functionals.

---
+++ testsuite/jobscripts/lda_xc/ORNL_Summit_LibXC_LDA_XC.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#BSUB -P      chm136                    # Project ID
+#BSUB -o      libxc.oe%J                # Job output name
+#BSUB -J      libxc_lda                 # Job name
+#BSUB -W      00:10                     # Walltime
+#BSUB -nnodes 1                         # Number of nodes
+#pwd
+#ls
+#env | sort
+export ISSUE_DIR=`pwd`
+export SCRATCH_DIR=$MEMBERWORK/chm136/$LSB_JOBNAME.$LSB_BATCH_JID
+export RUNLINE="jsrun --nrs=1 --rs_per_host=1 --cpu_per_rs=1 --gpu_per_rs=1"
+mkdir $SCRATCH_DIR
+cp ../../xc-regression           $SCRATCH_DIR
+cp ../../input/H                 $SCRATCH_DIR
+cp ../../input/Li                $SCRATCH_DIR
+cp ../../input/BrOH              $SCRATCH_DIR
+cp ../../input/BrOH+             $SCRATCH_DIR
+cd $SCRATCH_DIR
+#
+# xc-regression <functional> <nspin> <norder> <input> <output>
+#
+#    - functional: name
+#    - nspin     : integer number, 1 = unpolarized, 2 = polarized
+#    - norder    : integer number, order of derivatives <= 3
+#    - input     : input file name
+#    - output    : output file name
+#
+functionals=(
+  lda_xc_1d_ehwlrg_1
+  lda_xc_1d_ehwlrg_2
+  lda_xc_1d_ehwlrg_3
+  lda_xc_bn05
+  lda_xc_gdsmfb
+  lda_xc_ksdt
+  lda_xc_lp_a
+  lda_xc_lp_b
+  lda_xc_teter93
+  lda_xc_tih
+  lda_xc_zlp
+)
+#
+for func in "${functionals[@]}"; do
+    $RUNLINE ./xc-regression $func 1 0 H     ${func}.H.unpol.0.out 
+    $RUNLINE ./xc-regression $func 1 1 H     ${func}.H.unpol.1.out 
+    $RUNLINE ./xc-regression $func 1 2 H     ${func}.H.unpol.2.out 
+#
+    $RUNLINE ./xc-regression $func 2 0 H     ${func}.H.pol.0.out 
+    $RUNLINE ./xc-regression $func 2 1 H     ${func}.H.pol.1.out 
+    $RUNLINE ./xc-regression $func 2 2 H     ${func}.H.pol.2.out 
+#
+    $RUNLINE ./xc-regression $func 1 0 Li    ${func}.Li.unpol.0.out 
+    $RUNLINE ./xc-regression $func 1 1 Li    ${func}.Li.unpol.1.out 
+    $RUNLINE ./xc-regression $func 1 2 Li    ${func}.Li.unpol.2.out 
+#
+    $RUNLINE ./xc-regression $func 2 0 Li    ${func}.Li.pol.0.out 
+    $RUNLINE ./xc-regression $func 2 1 Li    ${func}.Li.pol.1.out 
+    $RUNLINE ./xc-regression $func 2 2 Li    ${func}.Li.pol.2.out 
+#
+    $RUNLINE ./xc-regression $func 1 0 BrOH  ${func}.BrOH.unpol.0.out 
+    $RUNLINE ./xc-regression $func 1 1 BrOH  ${func}.BrOH.unpol.1.out 
+    $RUNLINE ./xc-regression $func 1 2 BrOH  ${func}.BrOH.unpol.2.out 
+#
+    $RUNLINE ./xc-regression $func 2 0 BrOH  ${func}.BrOH.pol.0.out 
+    $RUNLINE ./xc-regression $func 2 1 BrOH  ${func}.BrOH.pol.1.out 
+    $RUNLINE ./xc-regression $func 2 2 BrOH  ${func}.BrOH.pol.2.out 
+#
+    $RUNLINE ./xc-regression $func 1 0 BrOH+ ${func}.BrOH+.unpol.0.out 
+    $RUNLINE ./xc-regression $func 1 1 BrOH+ ${func}.BrOH+.unpol.1.out 
+    $RUNLINE ./xc-regression $func 1 2 BrOH+ ${func}.BrOH+.unpol.2.out 
+#
+    $RUNLINE ./xc-regression $func 2 0 BrOH+ ${func}.BrOH+.pol.0.out 
+    $RUNLINE ./xc-regression $func 2 1 BrOH+ ${func}.BrOH+.pol.1.out 
+    $RUNLINE ./xc-regression $func 2 2 BrOH+ ${func}.BrOH+.pol.2.out 
+done
+#
+cp *.out  $ISSUE_DIR
+#
+rm -rf $SCRATCH_DIR
